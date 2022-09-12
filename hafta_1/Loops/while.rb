ea515d107a90a19482f5cfc934c5e12214b5e95e
Berkay Çelik <berkay@DESKTOP-OKGBARC.localdomain>
2022-09-12 09:38:52 +0300

    gereksiz dosyalar silindi

--- hafta_1/Loops/while.rb
+++
@@ -1,9 +0,0 @@
-i = 1
-
-while i <= 10
-  puts i
-  i *= 2
-end
-#Başlangıç değeri 1 olan i değeri while döngümüze girer.
-#Başlanğıç değerini ekrana yazdırdıktan sonra "i*=2"satırı çalışarak sayımız 2 ile çarpılır ve sonraki adıma geçer.
-#i değerimiz 16 olduğunda koşulu sağlamadıgı için döngüden çıkarak program son bulur.  
