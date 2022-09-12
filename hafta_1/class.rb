d020f4f7449a7941a819b3052be1c6d02a8ea3b0
Berkay Çelik <berkay@DESKTOP-OKGBARC.localdomain>
2022-09-12 11:06:01 +0300

    Güncelleme

--- hafta_1/class.rb
+++ hafta_1/class.rb
@@ -1,12 +1,11 @@
 class Ogrenci
-
     def ogrenci_bilgileri(adi,bolum,gano)
         @ogrenci_adi=adi
         @ogrenci_bolum=bolum
         @ogrenci_gano=gano
     end
 end
-o1 = Ogrenci.new
+o1 = Ogrenci.new        # burada Ogrenci sınıfından o1 adında bir nesne oluşturduk
 o1.ogrenci_bilgileri("Hasan","İnşaat",2.34)
 o2 = Ogrenci.new
 o2.ogrenci_bilgileri("ahmet","Eczacılık",3.15)
