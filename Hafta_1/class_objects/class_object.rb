1048721e310903b87c7bbc269deeed1afdf3469a
Furkan_ <furkan.akyol28@hotmail.com>
2022-09-12 10:53:30 +0300

    Son düzeltmeler yapıldı

---
+++ Hafta_1/class_objects/class_object.rb
@@ -0,0 +1,14 @@
+# ruby'de bir sınıf ve nesne üretmeyi görelim
+
+class Cikolata
+    def ye
+        puts "Tadı çok güzel!"
+    end
+end
+
+# Cikolata isimli bir class oluşturduk ve 'ye' isimli bir method tanımladık.
+
+my_cikolata = Cikolata.new
+my_cikolata.ye
+
+# my_cikolata isimli bir nesne oluşturduk ve 'ye' metodunu nesne üzerinden kullandık.
